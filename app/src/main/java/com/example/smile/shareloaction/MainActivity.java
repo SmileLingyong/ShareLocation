33515259bd307c143c0902f93277d2587fd7f657
smile <smilelingyong@163.com>
2017-10-13 21:11:23 +0800

    changed original SDK of book. The real-time positioning function is realized

--- app/src/main/java/com/example/smile/shareloaction/MainActivity.java
+++ app/src/main/java/com/example/smile/shareloaction/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.smile.shareloaction;
 
 import android.Manifest;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.StringBuilderPrinter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,23 +29,25 @@ public class MainActivity extends AppCompatActivity {
     public LocationClient mLocationClient;
     private TextView positionText;
     private MapView mapView;
-    private BaiduMap baiduMap;
+    private BaiduMap baiduMap;      // BaiduMap类是地图的总控制器
     private boolean isFirstLocate = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 创建了一个LocationClient的实例，
+        // 创建了一个LocationClient的实例，通过调用getApplicationContext()方法来获取一个全局的Context参数并传入。
         // 调用LocationClient的registerLocationListerner()方法来注册一个定位监听器，当获取到位置信息的时候，就会回调这个定位监听器。
         // 创建一个空的List集合，然后依次判断申请的3个权限有没有被授权，如果没有被授权就添加到List集合中，最后将List转换成数组，
         // 再调用ActivityCompat.requestPermissions()方法一次性申请。
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
-        SDKInitializer.initialize(getApplicationContext());
+        SDKInitializer.initialize(getApplicationContext()); // 初始化SDK，注意：该条语句一定要放在setContentView()方法调用前
+
         setContentView(R.layout.activity_main);
-        positionText = (TextView) findViewById(R.id.position_text_view);
         mapView = (MapView) findViewById(R.id.bmapView);
-        baiduMap = mapView.getMap();
+        baiduMap = mapView.getMap();            // 获取到BaiduMap的实例
+        baiduMap.setMyLocationEnabled(true);    // 开启显示用户当前位置在地图上的功能
+        positionText = (TextView) findViewById(R.id.position_text_view);
 
         List<String> permissionList = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -70,24 +68,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // 该函数功能：用于显示设备当前位置与地图上
+    private void navigateTo(BDLocation location) {
+        if (isFirstLocate) {
+            Toast.makeText(this, "nav to " + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+            // 获取经纬度信息，并将其存入LatLng对象之中，然后调用MapStatusUpdateFactory的newLatLng()方法将LatLng对象传入。
+            // 接着将返回的MapStatusUpdate对象，作为参数传入到BaiduMap的animateMapStatus()方法当中。
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            // 通过调用MapStatusUpdateFactory的zoomTo()方法，设置缩放级别，返回一个MapStatusUpdate对象
+            // 并将其作为参数传入到BaiduMap的animateMapStatus()方法当中，实现地图的缩放。
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            // isFirstLocate 该变量是为了防止多次调用animateMapStatus()方法，
+            // 因为将地图移动到我们当前位置只需要在程序第一次定位的时候调用一次就可以了
+            isFirstLocate = false;
+            Toast.makeText(this, "正在调用navigateTo()方法", Toast.LENGTH_SHORT).show();
+        }
+
+        // MyLocationData.Builder类是用来封装设备当前所在位置的，把要封装的信息都设置完毕后，
+        // 调用build()方法，会返回一个MyLocationData实例。
+        // 然后再将该实例传入到BaiduMap的setMyLocationData()方法中，就可以让设备当前位置显示在地图上了
+        MyLocationData.Builder locationBuilder = new MyLocationData.
+                Builder();
+        locationBuilder.latitude(location.getLatitude());
+        locationBuilder.longitude(location.getLongitude());
+        MyLocationData locationData = locationBuilder.build();
+        baiduMap.setMyLocationData(locationData);
+        Toast.makeText(this, "在地图上绘制设备位置", Toast.LENGTH_SHORT).show();
+    }
+
+
+    //开始定位
+    private void requestLocation() {
+        initLocation(); // 设置定位的相应参数，如定位更新时间、是否需要获取详细定位信息。
+        mLocationClient.start(); // 通过调用LocationClient的start()方法开始定位功能。
+    }
+
+
     // 在initLocation()方法中，我们创建了一个LocationClientOption对象，
     // 通过使用 setIsNeedAddress()方法，来开启获得当前位置更详细地址信息
     // 然后调用它的setScanSpan()方法来设置更新的间隔
-    public void initLocation() {
+    private void initLocation(){
         LocationClientOption option = new LocationClientOption();
-        option.setIsNeedAddress(true);
         option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
         mLocationClient.setLocOption(option);
     }
 
-    //设置地理位置定位
-    private void requestLocation() {
-//        LocationClientOption option = new LocationClientOption(); //创建一个LocationClientOption()对象 option
-//        option.setIsNeedAddress(true);  //设置需要获取当前位置详细信息（我们要获取 当前所在城市信息）
-//        mLocationClient.setLocOption(option);
-        initLocation();
-        mLocationClient.start();    //开始定位功能
-    }
 
     //对权限申请结果的逻辑处理
     @Override
@@ -112,54 +141,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    private void navigateTo(BDLocation location) {
-        if (isFirstLocate) {
-            Toast.makeText(this, "nav to " + location.getAddrStr(), Toast.LENGTH_SHORT).show();
-            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
-            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
-            baiduMap.animateMapStatus(update);
-            update = MapStatusUpdateFactory.zoomTo(16f);
-            baiduMap.animateMapStatus(update);
-            isFirstLocate = false;
-        }
-        MyLocationData.Builder locationBuilder = new MyLocationData.
-                Builder();
-        locationBuilder.latitude(location.getLatitude());
-        locationBuilder.longitude(location.getLongitude());
-        MyLocationData locationData = locationBuilder.build();
-        baiduMap.setMyLocationData(locationData);
-    }
-
-    public class MyLocationListener implements BDLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-
-//            StringBuilder currentPosition = new StringBuilder();
-//            currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
-//            currentPosition.append("经线：").append(location.getLongitude()).append("\n");
-//            currentPosition.append("国家：").append(location.getCountry()).append("\n");
-//            currentPosition.append("省：").append(location.getProvince()).append("\n");
-//            currentPosition.append("市：").append(location.getCity()).append("\n");
-//            currentPosition.append("区：").append(location.getDistrict()).append("\n");
-//            currentPosition.append("街道：").append(location.getStreet()).append("\n");
-//            currentPosition.append("定位方式：");
-//            if (location.getLocType() == BDLocation.TypeGpsLocation) {
-//                currentPosition.append("GPS");
-//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
-//                currentPosition.append("网络");
-//            }
-//            positionText.setText(currentPosition);
-
-            if (location.getLocType() == BDLocation.TypeGpsLocation
-                    || location.getLocType() == BDLocation.TypeNetWorkLocation) {
-                navigateTo(location);
-            }
-
-        }
-
-    }
-
+    // 重写 onResume()、onPause()、onDestory() 3个方法，对MapView进行管理，以保证资源能够及时的得到释放。
     @Override
     protected void onResume() {
         super.onResume();
@@ -177,5 +159,35 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         mLocationClient.stop();
         mapView.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
     }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override      // 在这个方法中，我们可以获取到丰富的地理位置信息
+        public void onReceiveLocation(BDLocation location) {
+//            StringBuilder currentPosition = new StringBuilder();
+//            currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
+//            currentPosition.append("经线：").append(location.getLongitude()).append("\n");
+//            currentPosition.append("国家：").append(location.getCountry()).append("\n");
+//            currentPosition.append("省：").append(location.getProvince()).append("\n");
+//            currentPosition.append("市：").append(location.getCity()).append("\n");
+//            currentPosition.append("区：").append(location.getDistrict()).append("\n");
+//            currentPosition.append("街道：").append(location.getStreet()).append("\n");
+//            currentPosition.append("定位方式：");
+//            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+//                currentPosition.append("GPS");
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+//                currentPosition.append("网络");
+//            }
+//            positionText.setText(currentPosition);
+            if (location.getLocType() == BDLocation.TypeGpsLocation
+                    || location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                navigateTo(location);
+            }
+        }
+
+    }
+
 }
+
